c3dbacc491139c8e9572bac7bfdbfa2189af6ad3
Joseph Brown <josephsbrown1999@hotmail.com>
2023-03-17 16:46:09 +0000

    Calendar Integration

--- app/src/main/java/com/example/assignment/AppHome.kt
+++ app/src/main/java/com/example/assignment/AppHome.kt
@@ -1,13 +1,24 @@
 package com.example.assignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_app_home)
+
+        val buttonClick = findViewById<Button>(R.id.open_cal)
+        buttonClick.setOnClickListener{
+            val intent = Intent(this, Calendar::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/assignment/Calendar.java
@@ -0,0 +1,137 @@
+package com.example.assignment;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class Calendar extends AppCompatActivity implements CalendarAdapter.OnItemListener
+{
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private LocalDate selectedDate;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        initWidgets();
+        selectedDate = LocalDate.now();
+        setMonthView();
+    }
+
+    private void initWidgets()
+    {
+        calendarRecyclerView = findViewById(R.id.calendar_recycler);
+        monthYearText = findViewById(R.id.MonthYearTV);
+    }
+
+    private void setMonthView()
+    {
+        monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+    private ArrayList<String> daysInMonthArray(LocalDate date)
+    {
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            yearMonth = YearMonth.from(date);
+        }
+
+        int daysInMonth = 0;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            daysInMonth = yearMonth.lengthOfMonth();
+        }
+
+        LocalDate firstOfMonth = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            firstOfMonth = selectedDate.withDayOfMonth(1);
+        }
+        int dayOfWeek = 0;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+        }
+
+        for(int i = 1; i <= 42; i++)
+        {
+            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek)
+            {
+                daysInMonthArray.add("");
+            }
+            else
+            {
+                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+        return  daysInMonthArray;
+    }
+
+    private String monthYearFromDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        }
+        return date.format(formatter);
+    }
+
+    public void previousMonthAction(View view)
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            selectedDate = selectedDate.minusMonths(1);
+        }
+        setMonthView();
+    }
+
+    public void nextMonthAction(View view)
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            selectedDate = selectedDate.plusMonths(1);
+        }
+        setMonthView();
+    }
+
+    @Override
+    public void onItemClick(int position, String dayText)
+    {
+        if(!dayText.equals(""))
+        {
+            String message = "Selected Date " + dayText + " " + monthYearFromDate(selectedDate);
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void return_home()
+    {
+        Button btn = (Button)findViewById(R.id.return_back);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Calendar.this, MainActivity2.class));
+            }
+        });
+    }
+}
